8b227e3583fe5eb63a51fc461f2f0e62957c19c9
Kourpidis Paris <kourpidisparis@gmail.com>
2025-06-28 11:30:04 +0300

    Complete Catalog Microservice

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,15 +1,24 @@
-﻿namespace Catalog.API.Products.DeleteProduct
+﻿using Catalog.API.Products.UpdateProduct;
+
+namespace Catalog.API.Products.DeleteProduct
 {
     public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
     public record DeleteProductResult(bool IsSuccess);
+
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+        }
+    }
+
     internal class DeleteProductHandler
-        (IDocumentSession session, ILogger<DeleteProductHandler> logger) 
+        (IDocumentSession session) 
         : ICommandHandler<DeleteProductCommand, DeleteProductResult>
     {
         public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
         {
-            logger.LogInformation("DeleteProductHandler.Handle called wtih {@Command}", command);
-
             session.Delete<Product>(command.Id);
             await session.SaveChangesAsync(cancellationToken);
 
